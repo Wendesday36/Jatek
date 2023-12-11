f8f9d2a678c0b2f33433d8a3570fc5dab149e26c
Wendesday36 <115298982+Wendesday36@users.noreply.github.com>
2023-12-11 11:41:07 +0100

    Feltoltes,megint mert mashova toltotte fel

---
+++ Jatek.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author berta.z.anna
+ */
+public class Jatek {
+   Karakter goblin = new Karakter(0, ugyesseg, 0)
+}
---
+++ Karakter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author berta.z.anna
+ */
+public class Karakter {
+    private int eletero;
+    private int ugyesseg;
+    private int szerencse;
+    private String[] felszereles;
+
+    public Karakter(int eletero, int ugyesseg, int szerencse) {
+        this.eletero = eletero;
+        this.ugyesseg = ugyesseg;
+        this.szerencse = szerencse;
+    }
+    
+}
