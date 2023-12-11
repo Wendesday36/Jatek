d30ad65f64652d58d58df4e885565ef11677cf37
Wendesday36 <bertazoeanna@gmail.com>
2023-12-11 08:48:42 +0100

    alap karakter es jatek

---
+++ src/game/Karakter.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author berta.z.anna
+ */
+public class Karakter {
+    private int eletero;
+    private String ugyesseg;
+    private int szerencse;
+
+    public Karakter(int eletero, String ugyesseg, int szerencse) {
+        this.eletero = eletero;
+        this.ugyesseg = ugyesseg;
+        this.szerencse = szerencse;
+    }
+    
+}
