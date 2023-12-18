4285996a367bbb45f432ad4921c9a7968565bed7
Wendesday36 <bertazoeanna@gmail.com>
2023-12-18 08:18:04 +0100

    a problema leirasa README-be

--- src/game/Jatek.java
+++ src/game/Jatek.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package game;
 
-/**
- *
- * @author berta.z.anna
- */
 public class Jatek {
    Karakter goblin = new Karakter(0, ugyesseg, 0)
 }
--- src/game/Karakter.java
+++ src/game/Karakter.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package game;
 
-/**
- *
- * @author berta.z.anna
- */
 public class Karakter {
     private int eletero;
     private int ugyesseg;
